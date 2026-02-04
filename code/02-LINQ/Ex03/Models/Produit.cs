f27f5780033c4bbb94f9a9477538074145a968ad
Etienne Caulier <152507490+ETIENNE-CLR@users.noreply.github.com>
2026-02-04 08:28:23 +0100

    ex 3

---
+++ code/02-LINQ/Ex03/Models/Produit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03_ProjectionSelect.Models
+{
+    internal class Produit
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public decimal PrixHT { get; set; }
+        public double TauxTVA { get; set; }
+        public string Categorie { get; set; }
+        public int Stock { get; set; }
+    }
+}
