dd78b571380c721db814b854d4f9e5e692d3134d
Etienne Caulier <152507490+ETIENNE-CLR@users.noreply.github.com>
2026-02-18 09:56:17 +0100

    deck controller

---
+++ code/SolitaireGame/Solitaire/Controllers/Deck.cs
@@ -0,0 +1,55 @@
+﻿using Solitaire.Enums;
+using Solitaire.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire.Controllers
+{
+    /// <summary>
+    /// Classe qui représente le deck de carte dans mon jeu
+    /// </summary>
+    internal class Deck
+    {
+        /// <summary>
+        /// Paquet de carte du jeu
+        /// </summary>
+        public List<Card> Cards { get; private set; }
+
+        /// <summary>
+        /// Liste des catégories de cartes
+        /// </summary>
+        private ImmutableList<CarteCategorie> categories;
+
+        /// <summary>
+        /// Constructeur de la classe...
+        /// </summary>
+        public Deck()
+        {
+            // Init
+            categories = ImmutableList.Create(
+                CarteCategorie.Clubs,
+                CarteCategorie.Diamonds,
+                CarteCategorie.Hearts,
+                CarteCategorie.Spades
+            );
+            GenerateDeck();
+        }
+
+        /// <summary>
+        /// Genère et retourne la liste des cartes
+        /// </summary>
+        /// <returns>Liste de <see cref="Card"/> générée</returns>
+        public List<Card> GenerateDeck()
+        {
+            // Générer les cartes du deck
+            for (int i = 0; i < categories.Count; i++)
+                for (int j = Card.MinValue; j <= Card.MaxValue; j++)
+                    Cards.Add(new Card(j, categories[i]));
+            return Cards;
+        }
+    }
+}
---
+++ code/SolitaireGame/Solitaire/Models/Tools.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire.Models
+{
+    static class Tools
+    {
+        /// <summary>
+        /// Fonction qui retourne un nombre aléatoire entre deux nombres compris
+        /// </summary>
+        /// <param name="min">Minimum de l'interval compris</param>
+        /// <param name="max">Maximum de l'interval compris</param>
+        /// <returns>Un nombre entier aléatoire</returns>
+        public static int GetRandomInt(int min, int max)
+        {
+            return new Random().Next(min, max + 1);
+        }
+
+        /// <summary>
+        /// Retourne la couleur entre vert et rouge d'un pourcentage
+        /// </summary>
+        /// <param name="healthPercentage">Le pourcentage</param>
+        /// <returns>La couleur représentant le pourcentage</returns>
+        public static Color GetHealthColor(float healthPercentage)
+        {
+            return Color.FromArgb(
+                255,                                 // Opacité max
+                (int)(255 * (1 - healthPercentage)), // Plus la vie baisse, plus le rouge augmente
+                (int)(255 * healthPercentage),       // Plus la vie baisse, plus le vert diminue
+                0                                    // Pas de bleu
+            );
+        }
+
+        /// <summary>
+        /// Récupère le chemin jusqu'au dossier de ressources
+        /// </summary>
+        /// <returns>Le chemin de ressources</returns>
+        public static string GetRessourcePath()
+        {
+            return $"{Tools.RepeatString("../", 2)}Resources/";
+        }
+
+        /// <summary>
+        /// Répète un certain nombre de fois un string
+        /// </summary>
+        /// <param name="text">Le texte à répéter</param>
+        /// <param name="n">Le nombre de fois qu'on doit le répéter</param>
+        /// <returns>Le string répété</returns>
+        public static string RepeatString(string text, int n)
+        {
+            return string.Concat(Enumerable.Repeat(text, (int)n));
+        }
+    }
+}
