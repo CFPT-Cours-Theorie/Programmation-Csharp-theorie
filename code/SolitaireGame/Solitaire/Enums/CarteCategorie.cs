46789caf90fcf4b877db7ff7168bf63151a7eb18
Etienne Caulier <152507490+ETIENNE-CLR@users.noreply.github.com>
2026-02-18 09:49:05 +0100

    class + enum card

---
+++ code/SolitaireGame/Solitaire/Enums/CarteCategorie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire.Enums
+{
+    /// <summary>
+    /// Enumération des différentes catégories de cartes
+    /// </summary>
+    internal enum CarteCategorie
+    {
+        /// <summary>
+        /// Groupe de trèfles
+        /// </summary>
+        Clubs,
+
+        /// <summary>
+        /// Groupe de carreaux
+        /// </summary>
+        Diamonds,
+
+        /// <summary>
+        /// Groupe de coeurs
+        /// </summary>
+        Hearts,
+
+        /// <summary>
+        /// Groupe de piques
+        /// </summary>
+        Spades
+    }
+}
---
+++ code/SolitaireGame/Solitaire/Models/Card.cs
@@ -0,0 +1,57 @@
+﻿using Solitaire.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire.Models
+{
+    /// <summary>
+    /// Classe représentant les cartes de jeu
+    /// </summary>
+    internal class Card
+    {
+        private int _value;
+
+        /// <summary>
+        /// Valeur de la carte
+        /// </summary>
+        public int Value { get => _value; private set => _value = Math.Clamp(value, MinValue, MaxValue); }
+
+        /// <summary>
+        /// Valeur minimale possible
+        /// </summary>
+        public const int MinValue = 1;
+
+        /// <summary>
+        /// Valeur maximale possible
+        /// </summary>
+        public const int MaxValue = 13;
+
+        /// <summary>
+        /// CarteCategorie de la carte
+        /// </summary>
+        public CarteCategorie Categorie { get; private set; }
+
+        /// <summary>
+        /// Constructeur de la classe....
+        /// </summary>
+        /// <param name="value">Valeur de la carte</param>
+        /// <param name="categorie">CarteCategorie de la carte</param>
+        public Card(int value, CarteCategorie categorie)
+        {
+            Value = value;
+            Categorie = categorie;
+        }
+
+        /// <summary>
+        /// Retourne le chemin de l'image de la carte
+        /// </summary>
+        /// <returns>Le nom du fichier image</returns>
+        public string GetFilePath()
+        {
+            return $"{Tools.GetRessourcePath()}{Categorie.ToString()}_{Value}.png"; ;
+        }
+    }
+}
